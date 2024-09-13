f620bb7081bad67a8893e9fc97661a361dcf51a3
Bianca Zatorre <biancazatorre@hotmail.com>
2024-09-12 21:27:32 -0300

    git put

--- contacorrente/src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
+++ contacorrente/src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +52,21 @@ public class ContaCorrenteController {
                             .buildAndExpand(conta.getId())
                             .toUri();
         return ResponseEntity.created(location).body(conta);
-        //HTTP 201 CREATED
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Conta> update(@RequestBody Conta conta){
+        if(contaService.update(conta)){
+            return ResponseEntity.ok(conta);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Conta> delete(@PathVariable Long id){
+        if(contaService.delete(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
